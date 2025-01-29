8a0ef9419db185081cf5bdb2e600a2984fc6bfc5
valhassan <victor.alhassan@canada.ca>
2025-01-29 17:25:59 -0500

    Refactor tiler.py: Improve tiling logic and add patch filtering

--- src/tiler.py
+++ src/tiler.py
@@ -1,9 +1,10 @@
-import pystac
 import logging
 import rasterio
-import geopandas as gpd
-from shapely.geometry import box
+import numpy as np
 import pandas as pd
+import geopandas as gpd
+
+from shapely.geometry import box
 from typing import Tuple, Dict, List
 from pathlib import Path
 
@@ -21,21 +22,21 @@ class Tiler:
     
     def __init__(self, 
                  input_pairs: List[Tuple[str, str]], 
-                 tile_size: Tuple[int, int],
+                 patch_size: Tuple[int, int], # (height, width)
                  attr_field: str,
                  attr_values: List[str],
-                 stride: Tuple[int, int],
-                 discard_empty: bool,
-                 label_threshold: float,
-                 single_class_mode: bool,
-                 multiclass_mode: Dict[str, bool],
-                 write_label_raster: bool,
-                 label_raster_path: str,
-                 output_dir: str):
+                 stride: int = None,
+                 discard_empty: bool = True,
+                 label_threshold: float = 0.01, # minimum of non-zero pixels in a patch to be considered valid (0-1)
+                 single_class_mode: bool = False,
+                 multiclass_mode: Dict[str, bool] = None,
+                 write_label_raster: bool = False,
+                 label_raster_path: str = None,
+                 output_dir: str = None):
         
         self.input_pairs = input_pairs
-        self.tile_size = tile_size
-        self.stride = stride
+        self.patch_size = patch_size
+        self.stride = stride if stride is not None else patch_size
         self.discard_empty = discard_empty
         self.label_threshold = label_threshold
         self.single_class_mode = single_class_mode
@@ -46,7 +47,33 @@ class Tiler:
         self.write_label_raster = write_label_raster
         self.label_raster_path = label_raster_path
         
+    @staticmethod
+    def pad_patch(patch: np.ndarray, patch_size: Tuple[int, int], mode='edge'):
+        """Pads the patch to the patch size"""
+        height, width = patch.shape[-2:]
+        pad_height = patch_size[0] - height
+        pad_width = patch_size[1] - width
+        if patch.ndim == 2:
+            padded_patch = np.pad(patch, ((0, pad_height), (0, pad_width)), mode=mode)
+            return padded_patch
+        elif patch.ndim == 3:
+            padded_patch = np.pad(patch, ((0, 0), (0, pad_height), (0, pad_width)), mode=mode)
+            return padded_patch
+        else:
+            raise ValueError(f"Invalid patch shape: {patch.shape}")
+    
+    
+    def _filter_patches(self, image: np.ndarray, label: np.ndarray):
+        """Filters patches based on the discard_empty flag and label_threshold"""
+        if self.discard_empty and np.all(label) == 0:
+            return False
         
+        if self.label_threshold:
+            label_percentage = np.sum(label > 0) / label.size
+            if label_percentage < self.label_threshold:
+                return False
+        return True
+    
     
     def _prepare_vector_labels(self, label: gpd.GeoDataFrame, image: rasterio.DatasetReader):
         """Prepares vector labels for tiling"""
@@ -58,26 +85,58 @@ class Tiler:
                                   write_raster=self.write_label_raster, 
                                   output_path=self.label_raster_path)
         return label
-        
-        
-    
-    # def _prepare_raster_labels(self, label: rasterio.DatasetReader, image: rasterio.DatasetReader):
-    #     """Prepares raster labels for tiling"""
-    #     nodata_mask = create_nodata_mask(image)
-    #     label = apply_nodata_mask_raster(label, nodata_mask, self.label_raster_path)
     
     def tiling(self, image: rasterio.DatasetReader, label: rasterio.DatasetReader):
         """Tiles the image and label"""
-        # intersection = image.read(1) & label.read(1)
-        pass
+        
+        image_width = image.width
+        image_height = image.height
+        
+        label_width = label.width
+        label_height = label.height
+        
+        assert (image_width == label_width and 
+                image_height == label_height), "Image and label dimensions must match"
+        assert (self.patch_size[0] <= image_height and 
+                self.patch_size[1] <= image_width), "Patch size must be smaller than image dimensions"
+        
+        image_patches = []
+        label_patches = []
+        
+        for y in range(0, image_height, self.stride):
+            for x in range(0, image_width, self.stride):
+                window_width = min(self.patch_size[1], image_width - x)
+                window_height = min(self.patch_size[0], image_height - y)
+                
+                window = rasterio.windows.Window(col_off=x, row_off=y, width=window_width, height=window_height)
+                image_patch = image.read(window=window, boundless=False)
+                label_patch = label.read(window=window, boundless=False)
+                
+                if window_width < self.patch_size[1] or window_height < self.patch_size[0]:
+                    image_patch = self.pad_patch(image_patch, self.patch_size)
+                    label_patch = self.pad_patch(label_patch, self.patch_size)
+                
+                if self._filter_patches(image_patch, label_patch):
+                    image_patches.append(image_patch)
+                    label_patches.append(label_patch)
+        
+        return image_patches, label_patches
+    
     
     def create_tiles(self):
         """Creates tiles from input pairs"""
         
         for input_pair in self.input_pairs:
+            
             image_path, label_path = input_pair
-            image = load_image(image_path)
-            label = load_mask(label_path)
+            
+            try:
+                image = load_image(image_path)
+                label = load_mask(label_path)
+            except Exception as e:
+                logger.error(f"Error loading image or label: {e}")
+                continue
+            
             if isinstance(label, gpd.GeoDataFrame) and (not is_image_georeferenced(image) or 
                     not is_label_georeferenced(label)):
                 logger.info("Skipping pair due to invalid label or image")
@@ -88,55 +147,35 @@ class Tiler:
                 if check_alignment(image, label):
                     logger.info("Tiling image and label pair")
                     self.tiling(image, label)
+                    continue
                 else:
                     logger.info("Skipping pair due to invalid label or image")
                     continue
             
-            else:
-                image_valid, image_msg = check_image_validity(image)
-                if not image_valid:
-                    print(f"Skipping pair due to invalid image: {image_msg}")
-                    continue
-                
-                label_valid, label_msg = check_label_validity(label)
-                if not label_valid:
-                    print(f"Skipping pair due to invalid label: {label_msg}")
-                    continue
-                
-                overlap_pct, overlap_msg = calculate_overlap(image, label)
-                if overlap_pct == 0:
-                    print(f"Skipping pair due to no overlap: {overlap_msg}")
-                    continue
-                else:
-                    print(f"Processing pair with {overlap_msg}")
-                
-                intersection = get_intersection(image, label)
-                if intersection is None:
-                    logger.info("Skipping pair: No intersection between image and label")
-                    continue
-                clipped_image, clipped_label = clip_to_intersection(image, label, intersection)
-                
-                print(f"image.crs: {image.crs}")
-                print(f"image.bounds: {box(*image.bounds)}")
-                print(f"image.count: {image.count}")
-                print(f"image.height: {image.height}")
-                print(f"image.width: {image.width}")
-                
-                print(f"clipped_image.crs: {clipped_image.crs}")
-                print(f"clipped_image.bounds: {box(*clipped_image.bounds)}")
-                print(f"clipped_image.count: {clipped_image.count}")
-                print(f"clipped_image.height: {clipped_image.height}")
-                print(f"clipped_image.width: {clipped_image.width}")
-                
-                
-                if isinstance(clipped_label, gpd.GeoDataFrame):
-                    
-                    label_bounds = clipped_label.total_bounds
-                    print(f"label_bounds: {box(*label_bounds)}")
-                    print(f"label.crs: {clipped_label.crs}")
-                    label = self._prepare_vector_labels(clipped_label, clipped_image)
-                    
-                    print(f"____label.bounds: {box(*label.bounds)}")
+            image_valid, image_msg = check_image_validity(image)
+            if not image_valid:
+                logger.info(f"Skipping pair due to invalid image: {image_msg}")
+                continue
+            
+            label_valid, label_msg = check_label_validity(label)
+            if not label_valid:
+                logger.info(f"Skipping pair due to invalid label: {label_msg}")
+                continue
+            
+            overlap_pct, overlap_msg = calculate_overlap(image, label)
+            if overlap_pct == 0:
+                logger.info(f"Skipping pair due to no overlap: {overlap_msg}")
+                continue
+            logger.info(f"Processing pair with {overlap_msg}")
+            
+            intersection = get_intersection(image, label)
+            if intersection is None:
+                logger.info("Skipping pair: No intersection between image and label")
+                continue
+            clipped_image, clipped_label = clip_to_intersection(image, label, intersection)
+            
+            if isinstance(clipped_label, gpd.GeoDataFrame):                    
+                label = self._prepare_vector_labels(clipped_label, clipped_image)
                     
                 
             # Continue with tiling...
@@ -158,8 +197,8 @@ if __name__ == '__main__':
                   single_class_mode=False, 
                   multiclass_mode={'class1': True, 'class2': False}, 
                   output_dir='/home/valhassa/Projects/geotiff-tiler/data/output',
-                  write_label_raster=False,
-                  label_raster_path='label.tif')
+                  write_label_raster=True,
+                  label_raster_path='final_label.tif')
     
     tiler.create_tiles()
     
